8b0367fc38a95c78ca85cfd5f48660fba72c904b
juemiao0814 <1429054526@qq.com>
2026-08-24 11:38:05 +0800

    Delete check.php

--- check.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include '../php/connect.php';
-$r=mysqli_query($conn,"SELECT * FROM admin WHERE username='$_POST[username]' AND password='$_POST[password]'");
-if(mysqli_num_rows($r)>0){$_SESSION['admin']=1;header('Location:index.php');}
-else{echo '账号密码错误';}
-?>
